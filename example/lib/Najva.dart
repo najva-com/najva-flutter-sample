4f87aeda9a6a97f61d00025f6ec79618890bc9eb
shayan darvish <shayan4shayan@gmail.com>
2019-07-30 08:42:43 +0000

    Update Najva.dart

--- example/lib/Najva.dart
+++ example/lib/Najva.dart
@@ -1,9 +1,9 @@
 import 'package:najvaflutter/najvaflutter.dart';
 
 class Najva extends NajvaFlutter {
-  int campaignId = 1247604; // your campaignId here
-  int websiteId = 5929; // your websiteId here
-  String apiKey = "ada0ff89-408e-4ffe-bf0b-2f1fb3ff77a1"; //your api key here
+  int campaignId = 0; // your campaignId here
+  int websiteId = 0; // your websiteId here
+  String apiKey = ""; //your api key here
 
   /// constructor for Najva
   Najva() {
