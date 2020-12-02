6c9d506d2ce80fecd15ef95e0f1d9d6e2c2c682d
Shayan <shayan4shayan@gmail.com>
2020-12-02 17:14:33 +0330

    sample updated to latest version

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:najvaflutter/najvaflutter.dart';
+import 'package:http/http.dart' as http;
 
-import 'Najva.dart';
+NajvaFlutter najva;
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  najva = new NajvaFlutter();
+  najva.init("8b84ad3a-3daa-4520-9adc-d7528ea95a54", 12383);
+  runApp(MyApp());
+}
 
-var map = [
-  [Colors.amber, Colors.amber, Colors.amber],
-  [Colors.amber, Colors.amber, Colors.amber],
-  [Colors.amber, Colors.amber, Colors.amber]
-];
 
 var player = true;
 
@@ -22,15 +26,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  Najva najva;
+
 
   Animation<double> animation;
   AnimationController controller;
 
+  String token = "Not available yet.";
+
   @override
   void initState() {
     super.initState();
-    najva = new Najva();
+    najva.setOnUserSubscribedListener(onUserSubscribed);
+    najva.getSubscribedToken().then((value) => {
+      setState(() {
+        token = value;
+      })
+    });
     controller =
         AnimationController(duration: const Duration(seconds: 1), vsync: this);
     animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
@@ -39,72 +50,10 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     });
   }
 
-  void onButtonClicked(int x, int y) {
-    if (map[x][y] != Colors.amber) {
-      return;
-    }
-    count++;
+  void onUserSubscribed(String token){
     setState(() {
-      if (player) {
-        map[x][y] = Colors.blue;
-      } else {
-        map[x][y] = Colors.red;
-      }
-
-      checkGame(x, y);
-
-      if (!gameDone) {
-        player = !player;
-        if (count >= 9) {
-          gameDone = true;
-          message = "Game has been a draw";
-          controller.forward();
-        }
-      } else {
-        message = "Player ${getPlayerNumber()} won the game";
-        controller.forward();
-      }
+      this.token = token;
     });
-  }
-
-  int getPlayerNumber() {
-    if (player) {
-      return 1;
-    } else {
-      return 2;
-    }
-  }
-
-  void checkGame(int x, int y) {
-    if (map[x][0] == map[x][1] && map[x][0] == map[x][2]) {
-      gameDone = true;
-    }
-    if (map[0][y] == map[1][y] && map[0][y] == map[2][y]) {
-      gameDone = true;
-    }
-    if (map[0][0] != Colors.amber &&
-        map[0][0] == map[1][1] &&
-        map[0][0] == map[2][2]) {
-      gameDone = true;
-    }
-    if (map[1][1] != Colors.amber &&
-        map[2][0] == map[1][1] &&
-        map[1][1] == map[0][2]) {
-      gameDone = true;
-    }
-  }
-
-  void resetGame() {
-    setState(() {
-      for (var row in map) {
-        for (var item in row) {
-          map[map.indexOf(row)][row.indexOf(item)] = Colors.amber;
-        }
-      }
-      gameDone = false;
-      player = true;
-    });
-    count = 0;
   }
 
   @override
@@ -112,175 +61,66 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Tic Toc Toe'),
+          title: const Text('Najva Sample app'),
         ),
         body: Center(child: getContent()),
       ),
     );
   }
 
+  void sendANotification(){
+    requestToServer().then((value) => {
+      print(value)
+    });
+  }
+
+  Future<String> requestToServer() async {
+    final response = await http.post(
+        "https://app.najva.com/notification/api/v1/notifications/",
+        headers: <String,String>{
+          "authorization": "Token aff3c68af97bf4608fdf28673ca26201ca027ed9",
+          "content-type": "application/json"
+        },
+        body: jsonEncode(<String,dynamic>{
+          "api_key":"8b84ad3a-3daa-4520-9adc-d7528ea95a54",
+          "title": "Notification from API",
+          "body": "this notification has been send from flutter sample",
+          "url":"https://najva.com",
+          "onclick-action":"open-link",
+          "priority": "high",
+          "subscriber_tokens": [token],
+        }));
+    return response.body;
+  }
+
   Widget getContent() {
-    if (gameDone) {
-      return Transform.translate(
-          child: Column(
+      return  Column(
             children: <Widget>[
-              Text(message),
-              RaisedButton(
-                child: Text("reset"),
-                onPressed: resetGame,
-              )
-            ],
-          ),
-          offset: Offset(0, animation.value));
-    } else {
-      return Card(
-        color: Colors.teal,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Flexible(
-                child: Container(
-                  height: 300,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80,
-                            child: RaisedButton(
-                              color: map[0][0],
-                              onPressed: () {
-                                onButtonClicked(0, 0);
-                              },
-                            ),
-                          ),
-                          flex: 2),
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80,
-                            child: RaisedButton(
-                              color: map[1][0],
-                              onPressed: () {
-                                onButtonClicked(1, 0);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80,
-                            child: RaisedButton(
-                              color: map[2][0],
-                              onPressed: () {
-                                onButtonClicked(2, 0);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                    ],
-                  ),
-                ),
-                flex: 1,
+
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(token),
               ),
+
               Flexible(
                 child: Container(
-                  height: 300,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80,
-                            child: RaisedButton(
-                              color: map[0][1],
-                              onPressed: () {
-                                onButtonClicked(0, 1);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80.0,
-                            child: RaisedButton(
-                              color: map[1][1],
-                              onPressed: () {
-                                onButtonClicked(1, 1);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80,
-                            child: RaisedButton(
-                              color: map[2][1],
-                              onPressed: () {
-                                onButtonClicked(2, 1);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                    ],
-                  ),
-                ),
-                flex: 1,
-              ),
-              Flexible(
-                child: Container(
-                  height: 300,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80,
-                            child: RaisedButton(
-                              color: map[0][2],
-                              onPressed: () {
-                                onButtonClicked(0, 2);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80.0,
-                            child: RaisedButton(
-                              color: map[1][2],
-                              onPressed: () {
-                                onButtonClicked(1, 2);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                      Flexible(
-                          child: ButtonTheme(
-                            height: 80.0,
-                            child: RaisedButton(
-                              color: map[2][2],
-                              onPressed: () {
-                                onButtonClicked(2, 2);
-                              },
-                            ),
-                          ),
-                          flex: 1),
-                    ],
-                  ),
+                  child: Flexible(
+                      child: ButtonTheme(
+                        height: 80,
+                        child: RaisedButton(
+                          color: Colors.blueAccent,
+                          onPressed: () {
+                            sendANotification();
+                          },
+                          child: Text("SEND A NOTIFICATION"),
+                        ),
+                      ),
+                      flex: 2)
                 ),
                 flex: 1,
               )
             ],
-          ),
-        ),
-      );
-    }
+          );
   }
 
   @override
